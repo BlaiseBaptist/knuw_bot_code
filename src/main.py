53d084aa1b2e21916bdcd7c7105bb8a9bf7695c9
BlaiseBaptist <bbaptist@thegraysonschool.org>
2024-09-19 16:54:23 -0400

    first commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       bbaptist                                                     #
+# 	Created:      9/19/2024, 4:52:59 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
