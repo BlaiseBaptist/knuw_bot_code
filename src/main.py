83771794bdea798b57d505d5e0cee4f1bbd0bd14
BlaiseBaptist <bbaptist@thegraysonschool.org>
2024-09-24 16:00:19 -0400

    did things
    sorry rena

--- src/main.py
+++ src/main.py
@@ -19,8 +19,29 @@ left_18 = Motor(Ports.PORT18, GearSetting.RATIO_6_1,False)
 right_12 = Motor(Ports.PORT12, GearSetting.RATIO_6_1,False)
 right_11 = Motor(Ports.PORT11, GearSetting.RATIO_6_1,False)
 right_13 = Motor(Ports.PORT13, GearSetting.RATIO_6_1,False)
-
+lock = DigitalOut(brain.three_wire_port.b)
+grab = DigitalOut(brain.three_wire_port.a)
 motors = {"left_16":left_16,"left_19":left_19,"left_18":left_18,"right_12":right_12,"right_11":right_11,"right_13":right_13}
 
 left_group = MotorGroup(left_18,left_16,left_19)
 right_group = MotorGroup(right_11,right_12,right_13)
+
+big_c = Controller(PRIMARY)
+def one_stick():
+    while True:
+        throttle = big_c.axis3.position()
+        turn = big_c.axis1.position()
+        left = throttle-turn
+        right = throttle+turn
+        left_group.spin(FORWARD,left,PERCENT)
+        right_group.spin(FORWARD,right,PERCENT)
+    
+def driver():
+    Thread(one_stick)
+    big_c.buttonA.pressed(lambda: grab.set(not grab.value()))
+    big_c.buttonB.pressed(lambda: lock.set(not lock.value()))
+
+def auton():
+    pass
+
+c = Competition(driver,auton)
