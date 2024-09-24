f56d06369b0a5c7dab1179e56499ffbabbb95849
BlaiseBaptist <bbaptist@thegraysonschool.org>
2024-09-24 16:25:47 -0400

    fixed motor direction

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # 	Module:       main.py                                                      #
-# 	Author:       bbaptist                                                     #
+# 	Author:       blase baptist                                                #
 # 	Created:      9/19/2024, 4:52:59 PM                                        #
 # 	Description:  V5 project                                                   #
 #                                                                              #
@@ -13,9 +13,9 @@ from vex import *
 # Brain should be defined by default
 brain=Brain()
 
-left_16 = Motor(Ports.PORT16, GearSetting.RATIO_6_1,False)
-left_19 = Motor(Ports.PORT19, GearSetting.RATIO_6_1,False)
-left_18 = Motor(Ports.PORT18, GearSetting.RATIO_6_1,False)
+left_16 = Motor(Ports.PORT16, GearSetting.RATIO_6_1,True)
+left_19 = Motor(Ports.PORT19, GearSetting.RATIO_6_1,True)
+left_18 = Motor(Ports.PORT18, GearSetting.RATIO_6_1,True)
 right_12 = Motor(Ports.PORT12, GearSetting.RATIO_6_1,False)
 right_11 = Motor(Ports.PORT11, GearSetting.RATIO_6_1,False)
 right_13 = Motor(Ports.PORT13, GearSetting.RATIO_6_1,False)
@@ -27,10 +27,11 @@ left_group = MotorGroup(left_18,left_16,left_19)
 right_group = MotorGroup(right_11,right_12,right_13)
 
 control = Controller(PRIMARY)
+code = Controller()
 def one_stick():
     while True:
         throttle = control.axis3.position()
-        turn = control.axis1.position()
+        turn = control.axis4.position()
         left = throttle-turn
         right = throttle+turn
         left_group.spin(FORWARD,left,PERCENT)
